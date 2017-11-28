7d080885d253228badd78006c405d6981298a184
Treesoon\DAFT <treesoon.dev@goingstudio.com>
2017-11-28 16:49:10 +0700

    ส่งครั้งที่ 1 เหลือ responsive ครับ

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# SaneCar
+# BaseTheme
+Going Up Theme
+Going Design Studio Base Theme
+
+ไฟล์ WordPress พื้นฐานสำหรับ Going Design Stido
+# Going-Up
---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($) {
+    new WOW({
+        scrollContainer: '#page'
+    }).init();
+
+    // //Slidebars
+    // var controller = new slidebars();
+    // controller.init();
+
+    // // Toggle Slidebars
+    // $('.toggle-menu').on('click', function(event) {
+    //     // Stop default action and bubbling
+    //     event.stopPropagation();
+    //     event.preventDefault();
+
+    //     // Toggle the Slidebar with id 'id-1'
+    //     controller.toggle('page-sidebar');
+    // });
+
+});
